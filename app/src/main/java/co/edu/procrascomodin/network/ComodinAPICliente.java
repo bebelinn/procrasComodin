5af3146a5ad85c3e2b7fb3bdef6824fe31b83cc8
bebelinn <145699248+bebelinn@users.noreply.github.com>
2024-02-13 11:31:43 -0500

    comodin nuevo

--- app/src/main/java/co/edu/procrascomodin/network/ComodinAPICliente.java
+++ app/src/main/java/co/edu/procrascomodin/network/ComodinAPICliente.java
@@ -1,4 +1,22 @@
 package co.edu.procrascomodin.network;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ComodinAPICliente {
+    private static final String URL = "http://10.201.194.22:8000/api/";
+
+    private static ComodinAPIService instance;
+
+    public static ComodinAPIService getComodinInstance() {
+        if (instance ==null){
+            Retrofit retro = new Retrofit.Builder()
+                    .baseUrl(URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            instance = retro.create(ComodinAPIService.class);
+        }
+        return instance;
+    }
+
 }
--- app/src/main/java/co/edu/procrascomodin/network/ComodinAPIService.java
+++ app/src/main/java/co/edu/procrascomodin/network/ComodinAPIService.java
@@ -1,4 +1,22 @@
-package co.edu.procrascomodin.network;public interface ComodinAPIService {
+package co.edu.procrascomodin.network;
 
+import java.util.List;
 
+import co.edu.procrascomodin.Comodin;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface ComodinAPIService {
+    @GET ("comodin")
+    Call<List<Comodin>>getAll();
+    @GET ("comodin/{id}")
+    Call<Comodin> get(@Path("id")long id);
+    @POST ("comodin")
+    Call<Comodin> add (@Body Comodin comodin);
+    @DELETE ("ficha/{id}")
+    Call <Comodin> delete(@Path("id")long id, @Body Comodin comodin);
 }
